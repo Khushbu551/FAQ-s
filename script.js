94f52dcf3d9ebc4316774f7589d8577ccc4b2f08
Khushbu551 <khushbu.patel@dci-student.org>
2024-12-04 14:58:09 +0100

    FAQ'S

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const accordians = document.querySelectorAll(".accordian");
 
-function myFunction() {
-  var element = document.getElementById("myDIV");
-  element.classList.toggle("mystyle");
-}
 accordians.forEach((accordian) => {
-  const icon = document.querySelector(".icon");
-  const answer = document.querySelector(".answer");
-  console.log(icon, "icons");
-  console.log(answer, "div");
-  accordian.addEventListener(".click", () => {
+  const icon = accordian.querySelector(".icon");
+  const answer = accordian.querySelector(".answer");
+
+  accordian.addEventListener("click", () => {
     icon.classList.toggle("active");
     answer.classList.toggle("active");
   });
